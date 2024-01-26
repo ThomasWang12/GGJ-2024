10e58f608fc50fd5ed34306da7f31fcc7eeaf40a
Thomas Wang <73792618+ThomasWang12@users.noreply.github.com>
2024-01-26 23:49:45 +0800

    Before Refactor (Minor)

--- MakeMeLaugh/Assets/Scripts/Scene_01.cs
+++ MakeMeLaugh/Assets/Scripts/Scene_01.cs
@@ -6,33 +6,30 @@ using UnityEngine;
 public class Scene_01 : MonoBehaviour
 {
     [SerializeField] Master master;
-    [SerializeField] Player player;
+    [SerializeField] Player_01 player;
+    public GameObject bed;
     [SerializeField] GameObject[] rainSprite;
+    public GameObject[] sleepSprite;
 
-    public enum State
-    {
-        Init, Sleep, Walk
-    }
-    public State State_01;
+    public enum State { Init, Sleep, Walk }
+    public State state;
 
     void FixedUpdate()
     {
         // Rain animation in background
         Common.Anim(rainSprite);
 
-        switch (State_01)
+        switch (state)
         {
             case State.Init:
                 player.Scene01_Init();
                 break;
             case State.Sleep:
-                if (Input.GetMouseButtonDown(0))
-                    player.Scene01_Input();
+                player.Scene01_Input();
                 player.Scene01_Sleep();
                 break;
             case State.Walk:
-                if (Input.GetMouseButtonDown(0))
-                    player.Scene01_Input();
+                player.Scene01_Input();
                 player.Scene01_Walk();
                 break;
         }
